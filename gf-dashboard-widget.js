b9031c026d1c3161a7f152c85c2e02fa5ff93def
DigitalBrent <blackbowrodent@gmail.com>
2023-09-26 22:50:19 -0400

    devgpt not looking so stellar rn

--- gf-dashboard-widget.js
+++ gf-dashboard-widget.js
@@ -1,34 +1,30 @@
 document.addEventListener('DOMContentLoaded', function () {
     const dropdown = document.getElementById('gf_forms_dropdown');
     const displayArea = document.getElementById('gf_emails_display');
-    const downloadSection = document.querySelector('.gf-report-section');
-    const startDateInput = document.getElementById('gf_start_date');
-    const endDateInput = document.getElementById('gf_end_date');
-    const downloadBtn = document.querySelector('.gf-download-btn');
 
     function fetchDataForForm(formId) {
         // Use AJAX to fetch the data
         // Fetch the entries count for today
-        fetch(gfDashboardWidget.ajaxurl, {
+        fetch(ajaxurl, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/x-www-form-urlencoded',
             },
-            body: 'action=get_form_entries_today&form_id=' + encodeURIComponent(formId)
+            body: 'action=get_form_entries_today&form_id=' + formId
         })
             .then(response => response.json())
-			.then(data => {
+            .then(data => {
                 const entriesDisplay = document.getElementById('gf_entries_today_display');
-                entries.textContent = "Entries Today: " + data.count;
+                entriesDisplay.textContent = "Entries Today: " + data.count;
             });
 
 
-        fetch(gfDashboardWidget.ajaxurl, {
+        fetch(ajaxurl, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/x-www-form-urlencoded',
             },
-            body: 'action=get_form_emails&form_id=' + encodeURIComponent(formId)
+            body: 'action=get_form_emails&form_id=' + formId
         })
             .then(response => response.json())
             .then(data => {
@@ -42,18 +38,4 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Fetch data for the initial form on page load
     fetchDataForForm(dropdown.value);
-
-    // Handle download button click
-    downloadBtn.addEventListener('click', function () {
-        const startDate = startDateInput.value;
-        const endDate = endDateInput.value;
-        const formId = dropdown.value;
-
-        // Redirect to download URL
-        window.location.href = gfDashboardWidget.ajaxurl +
-            '?action=download_form_entries' +
-            '&form_id=' + encodeURIComponent(formId) +
-            '&start_date=' + encodeURIComponent(startDate) +
-            '&end_date=' + encodeURIComponent(endDate);
-    });
 });
